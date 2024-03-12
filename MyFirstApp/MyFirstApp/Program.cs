4616e1cebc6afa26b4eb6d0e8c882da8713c35e9
Aleksandra Filipowicz <a.filipowicz12345@gmail.com>
2024-03-12 11:33:37 +0100

    Calculate Average

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,4 +1,20 @@
-﻿git status// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
+
+int[] numbers = {1,2,3,4};
+double average = GetAverage(numbers);
+Console.WriteLine(average);
