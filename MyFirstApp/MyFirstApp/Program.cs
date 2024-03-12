e3a7aa7ba935fd04e5481c708679e5175d5ba4fb
Aleksandra Filipowicz <a.filipowicz12345@gmail.com>
2024-03-12 11:36:14 +0100

    delete line

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,4 +1,4 @@
-﻿git status// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
